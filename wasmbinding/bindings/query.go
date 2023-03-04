64c721b076cf7ed40ce29a35cde4c5cadefe4f13
Till Ziegler <tz@schoeneweide.tk>
2023-03-04 10:10:14 +0100

    add wasm-bindings for canonical wasmd

---
+++ wasmbinding/bindings/query.go
@@ -0,0 +1,22 @@
+package bindings
+
+import(
+	markettypes "github.com/classic-terra/core/x/market/types"
+	oracletypes "github.com/classic-terra/core/x/oracle/types"
+	treasurytypes "github.com/classic-terra/core/x/treasury/types"
+)
+
+// ExchangeRateQueryParams query request params for exchange rates
+type ExchangeRateQueryParams struct {
+	BaseDenom   string   `json:"base_denom"`
+	QuoteDenoms []string `json:"quote_denoms"`
+}
+
+// TerraQuery contains terra custom queries.
+type TerraQuery struct {
+	Swap *markettypes.QuerySwapParams `json:"swap,omitempty"`
+	ExchangeRates *ExchangeRateQueryParams `json:"exchange_rates,omitempty"`
+	TaxRate *struct{}                `json:"tax_rate,omitempty"`
+	TaxCap  *treasurytypes.QueryTaxCapParams `json:"tax_cap,omitempty"`
+}
+

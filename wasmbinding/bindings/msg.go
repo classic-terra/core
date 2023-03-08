479f615662af5d368aa00d606ded5ee896abeb7c
Till Ziegler <tz@schoeneweide.tk>
2023-03-08 22:11:16 +0100

    add wasm-bindings for canonical wasmd (#164)
    Add folder wasmbinding and wasmbinding/bindings which contain bindings for custom market/oracle/treasury queries and messages for wasm/wasmd.

---
+++ wasmbinding/bindings/msg.go
@@ -0,0 +1,11 @@
+package bindings
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	markettypes "github.com/classic-terra/core/x/market/types"
+)
+
+type TerraMsg struct {
+	Swap     *markettypes.MsgSwap     `json:"swap,omitempty"`
+	SwapSend *markettypes.MsgSwapSend `json:"swap_send,omitempty"`
+}

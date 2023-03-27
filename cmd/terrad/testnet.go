9eed2de02c08ff8261035fc99cbec0c38fdd4e9f
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2023-03-27 12:55:56 +0700

    change voting period to 30s

--- cmd/terrad/testnet.go
+++ cmd/terrad/testnet.go
@@ -310,7 +310,7 @@ func initGenFiles(
 	// set gov in the genesis state
 	var govGenState govtypes.GenesisState
 	clientCtx.Codec.MustUnmarshalJSON(appGenState[govtypes.ModuleName], &govGenState)
-	govGenState.VotingParams.VotingPeriod = time.Minute * 5
+	govGenState.VotingParams.VotingPeriod = time.Second * 30
 	appGenState[govtypes.ModuleName] = clientCtx.Codec.MustMarshalJSON(&govGenState)
 
 	appGenStateJSON, err := json.MarshalIndent(appGenState, "", "  ")

fa01d644b8dcefb2608cd29e03437a58efeff4bb
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2023-04-21 12:40:07 +0700

    remove redundant temp.sh file

--- temp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-terrad q staking validator $(terrad keys show test0 -a --bech val --keyring-backend test --home $HOME) >/dev/null 2>&1
-
-if [ $? -eq 0 ]; then
-    echo "Success"
-else
-    echo "Failed"
-fi

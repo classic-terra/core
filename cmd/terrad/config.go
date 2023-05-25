88e2f4f32b6f1938b6f82232ce76f5a20e6dfc03
Till Ziegler <tz@schoeneweide.tk>
2023-05-25 20:52:03 +0200

    add default wasm config to init cmd

--- cmd/terrad/config.go
+++ cmd/terrad/config.go
@@ -1,16 +1,76 @@
 package main
 
 import (
+	"fmt"
 //	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	serverconfig "github.com/cosmos/cosmos-sdk/server/config"
-//	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 )
+
+const (
+	defaultMemoryCacheSize    uint32 = 100 // in MiB
+	defaultSmartQueryGasLimit uint64 = 3_000_000
+	defaultContractDebugMode         = false
+)
+
+
+type WasmConfig struct {
+	// SimulationGasLimit is the max gas to be used in a tx simulation call.
+	// When not set the consensus max block gas is used instead
+	SimulationGasLimit *uint64 `mapstructure:"simulation_gas_limit"`
+	// SmartQueryGasLimit is the max gas to be used in a smart query contract call
+	SmartQueryGasLimit uint64 `mapstructure:"query_gas_limit"`
+	// MemoryCacheSize in MiB not bytes
+	MemoryCacheSize uint32 `mapstructure:"memory_cache_size"`
+	// ContractDebugMode log what contract print
+	ContractDebugMode bool
+}
 
 // TerraAppConfig terra specify app config
 type TerraAppConfig struct {
 	serverconfig.Config
+	Wasm wasmtypes.WasmConfig `mapstructure:"wasm"`
+}
 
-	//WASMConfig wasmtypes.WasmConfig `mapstructure:"wasm"`
+// DefaultWasmConfig returns the default settings for WasmConfig
+func DefaultWasmConfig() WasmConfig {
+	return WasmConfig{
+		SmartQueryGasLimit: defaultSmartQueryGasLimit,
+		MemoryCacheSize:    defaultMemoryCacheSize,
+		ContractDebugMode:  defaultContractDebugMode,
+	}
+}
+
+// ConfigTemplate toml snippet for app.toml
+func WasmConfigTemplate(c WasmConfig) string {
+	simGasLimit := `# simulation_gas_limit =`
+	if c.SimulationGasLimit != nil {
+		simGasLimit = fmt.Sprintf(`simulation_gas_limit = %d`, *c.SimulationGasLimit)
+	}
+
+	return fmt.Sprintf(`
+
+###############################################################################
+###                                  WASM                                   ###
+###############################################################################
+
+[wasm]
+# Smart query gas limit is the max gas to be used in a smart query contract call
+query_gas_limit = %d
+
+# in-memory cache for Wasm contracts. Set to 0 to disable.
+# The value is in MiB not bytes
+memory_cache_size = %d
+
+# Simulation gas limit is the max gas to be used in a tx simulation call.
+# When not set the consensus max block gas is used instead
+%s
+`, c.SmartQueryGasLimit, c.MemoryCacheSize, simGasLimit)
+}
+
+// DefaultConfigTemplate toml snippet with default values for app.toml
+func DefaultWasmConfigTemplate() string {
+	return WasmConfigTemplate(DefaultWasmConfig())
 }
 
 // initAppConfig helps to override default appConfig template and configs.
@@ -36,10 +96,10 @@ func initAppConfig() (string, interface{}) {
 
 	terraAppConfig := TerraAppConfig{
 		Config:     *srvCfg,
-		//WASMConfig: wasmtypes.DefaultWasmConfig(),
+		Wasm: wasmtypes.DefaultWasmConfig(),
 	}
 
-	terraAppTemplate := serverconfig.DefaultConfigTemplate //+ wasmconfig.DefaultConfigTemplate
+	terraAppTemplate := serverconfig.DefaultConfigTemplate + DefaultWasmConfigTemplate()
 
 	return terraAppTemplate, terraAppConfig
 }

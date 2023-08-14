960fbec8d482a4c4185a0a06d809e5058581c9fb
Inon Man <121477599+inon-man@users.noreply.github.com>
2023-08-14 13:02:08 +0900

    add ibcfee and ibccontroller store keys to StoreUpgrades

--- app/upgrades/v5/constants.go
+++ app/upgrades/v5/constants.go
@@ -3,6 +3,9 @@ package v5
 import (
 	"github.com/classic-terra/core/v2/app/upgrades"
 	store "github.com/cosmos/cosmos-sdk/store/types"
+
+	icacontrollertypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/controller/types"
+	ibcfeetypes "github.com/cosmos/ibc-go/v6/modules/apps/29-fee/types"
 )
 
 const UpgradeName = "v5"
@@ -10,5 +13,10 @@ const UpgradeName = "v5"
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateV5UpgradeHandler,
-	StoreUpgrades:        store.StoreUpgrades{},
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{
+			ibcfeetypes.StoreKey,
+			icacontrollertypes.StoreKey,
+		},
+	},
 }

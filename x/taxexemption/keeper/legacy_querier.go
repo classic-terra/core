6b1d0b806c238284e7077d44054db6dbc613b156
StrathCole <strathcole@gmail.com>
2023-11-28 12:25:34 +0100

    - linting fix

--- x/taxexemption/keeper/legacy_querier.go
+++ x/taxexemption/keeper/legacy_querier.go
@@ -12,9 +12,6 @@ import (
 // NewLegacyQuerier is the module level router for state queries
 func NewLegacyQuerier(k Keeper, legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err error) {
-		switch path[0] {
-		default:
-			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unknown %s query endpoint: %s", types.ModuleName, path[0])
-		}
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unknown %s query endpoint: %s", types.ModuleName, path[0])
 	}
 }

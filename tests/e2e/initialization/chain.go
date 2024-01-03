91f347fcf9d791caed8f0caf032c75d97248db82
Pham Anh Minh <1phamminh0811@gmail.com>
2024-01-03 14:11:56 +0700

    Add chain to e2e

--- tests/e2e/initialization/chain.go
+++ tests/e2e/initialization/chain.go
@@ -1,1 +1,40 @@
 package initialization
+
+import (
+	"github.com/tendermint/tendermint/p2p"
+	"github.com/tendermint/tendermint/privval"
+
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+)
+
+type ChainMeta struct {
+	DataDir string
+	Id      string
+}
+
+type Validator struct {
+	Chain            *Chain
+	Index            int
+	Moniker          string
+	Mnemonic         string
+	PublicAddress    string
+	PublicKey        cryptotypes.PubKey
+	PrivateKey       cryptotypes.PrivKey
+	ConsensusKey     privval.FilePVKey
+	ConsensusPrivKey cryptotypes.PrivKey
+	NodeKey          p2p.NodeKey
+}
+
+type Node struct {
+	Moniker       string //nolint:unused
+	Mnemonic      string
+	PublicAddress string
+	PublicKey     cryptotypes.PubKey
+	PrivateKey    cryptotypes.PrivKey
+}
+
+type Chain struct {
+	ChainMeta ChainMeta
+	Validator []*Validator
+	Node	  []*Node
+}

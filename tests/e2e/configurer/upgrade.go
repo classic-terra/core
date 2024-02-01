f8b8832719c5358377fc86d689eb40689105469b
Pham Anh Minh <1phamminh0811@gmail.com>
2024-02-01 11:46:44 +0700

    lint

--- tests/e2e/configurer/upgrade.go
+++ tests/e2e/configurer/upgrade.go
@@ -148,7 +148,7 @@ func (uc *UpgradeConfigurer) runProposalUpgrade() error {
 					return err
 				}
 				chainConfig.UpgradePropHeight = currentHeight + int64(chainConfig.VotingPeriod) + int64(config.PropSubmitBlocks) + int64(config.PropBufferBlocks)
-				node.SubmitUpgradeProposal(uc.upgradeVersion, chainConfig.UpgradePropHeight, sdk.NewCoin(initialization.TerraDenom, sdk.NewInt(config.MinDepositValue * assets.MicroUnit/4)))
+				node.SubmitUpgradeProposal(uc.upgradeVersion, chainConfig.UpgradePropHeight, sdk.NewCoin(initialization.TerraDenom, sdk.NewInt(config.MinDepositValue*assets.MicroUnit/4)))
 				chainConfig.LatestProposalNumber++
 				node.DepositProposal(chainConfig.LatestProposalNumber, false)
 			}

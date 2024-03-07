ec9105c0e79daa11c7a154f59d3dd3e64398a46c
Till Ziegler <tz@schoeneweide.tk>
2024-03-07 09:58:14 +0100

    fix: initial deposit for v1 props (#444)
    Co-authored-by: StrathCole <7449529+StrathCole@users.noreply.github.com>

--- custom/auth/ante/min_initial_deposit.go
+++ custom/auth/ante/min_initial_deposit.go
@@ -7,6 +7,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
+	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	govv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 )
 
@@ -26,24 +27,32 @@ func NewMinInitialDepositDecorator(govKeeper govkeeper.Keeper, treasuryKeeper Tr
 
 // IsMsgSubmitProposal checks whether the input msg is a MsgSubmitProposal
 func IsMsgSubmitProposal(msg sdk.Msg) bool {
-	_, ok := msg.(*govv1beta1.MsgSubmitProposal)
-	return ok
+	switch msg.(type) {
+	case *govv1beta1.MsgSubmitProposal, *govv1.MsgSubmitProposal:
+		return true
+	default:
+		return false
+	}
 }
 
 // HandleCheckMinInitialDeposit
 func HandleCheckMinInitialDeposit(ctx sdk.Context, msg sdk.Msg, govKeeper govkeeper.Keeper, treasuryKeeper TreasuryKeeper) (err error) {
-	submitPropMsg, ok := msg.(*govv1beta1.MsgSubmitProposal)
-	if !ok {
+	var initialDepositCoins sdk.Coins
+
+	switch submitPropMsg := msg.(type) {
+	case *govv1beta1.MsgSubmitProposal:
+		initialDepositCoins = submitPropMsg.GetInitialDeposit()
+	case *govv1.MsgSubmitProposal:
+		initialDepositCoins = submitPropMsg.GetInitialDeposit()
+	default:
 		return fmt.Errorf("could not dereference msg as MsgSubmitProposal")
 	}
-
 	minDeposit := govKeeper.GetDepositParams(ctx).MinDeposit
 	requiredAmount := sdk.NewDecFromInt(minDeposit[0].Amount).Mul(treasuryKeeper.GetMinInitialDepositRatio(ctx)).TruncateInt()
 
 	requiredDepositCoins := sdk.NewCoins(
 		sdk.NewCoin(core.MicroLunaDenom, requiredAmount),
 	)
-	initialDepositCoins := submitPropMsg.GetInitialDeposit()
 
 	if !initialDepositCoins.IsAllGTE(requiredDepositCoins) {
 		return fmt.Errorf("not enough initial deposit provided. Expected %q; got %q", requiredDepositCoins, initialDepositCoins)

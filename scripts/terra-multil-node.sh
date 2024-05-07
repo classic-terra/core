eefe225f4402d9aec5131adbff189d94d8d58e25
Dong Lieu <duongduydongtq@gmail.com>
2024-05-07 11:26:46 +0700

    update script

--- scripts/terra-multil-node.sh
+++ scripts/terra-multil-node.sh
@@ -40,8 +40,8 @@ cp validator2/config/gentx/*.json $HOME/.terrad/validator1/config/gentx/
 cp validator3/config/gentx/*.json $HOME/.terrad/validator1/config/gentx/
 terrad collect-gentxs --home=$HOME/.terrad/validator1 
 
-cp validator1/config/genesis.json $HOME/.terrad/validator2/config/genesis.json
-cp validator1/config/genesis.json $HOME/.terrad/validator3/config/genesis.json
+# cp validator1/config/genesis.json $HOME/.terrad/validator2/config/genesis.json
+# cp validator1/config/genesis.json $HOME/.terrad/validator3/config/genesis.json
 
 
 # change app.toml values
@@ -54,18 +54,18 @@ sed -i -E 's|0.0.0.0:9090|0.0.0.0:9050|g' $VALIDATOR1_APP_TOML
 
 # validator2
 sed -i -E 's|tcp://localhost:1317|tcp://localhost:1316|g' $VALIDATOR2_APP_TOML
-# sed -i -E 's|0.0.0.0:9090|0.0.0.0:9088|g' $VALIDATOR2_APP_TOML
-sed -i -E 's|localhost:9090|localhost:9088|g' $VALIDATOR2_APP_TOML
-# sed -i -E 's|0.0.0.0:9091|0.0.0.0:9089|g' $VALIDATOR2_APP_TOML
-sed -i -E 's|localhost:9091|localhost:9089|g' $VALIDATOR2_APP_TOML
+sed -i -E 's|0.0.0.0:9090|0.0.0.0:9088|g' $VALIDATOR2_APP_TOML
+# sed -i -E 's|localhost:9090|localhost:9088|g' $VALIDATOR2_APP_TOML
+sed -i -E 's|0.0.0.0:9091|0.0.0.0:9089|g' $VALIDATOR2_APP_TOML
+# sed -i -E 's|localhost:9091|localhost:9089|g' $VALIDATOR2_APP_TOML
 sed -i -E 's|tcp://0.0.0.0:10337|tcp://0.0.0.0:10347|g' $VALIDATOR2_APP_TOML
 
 # validator3
 sed -i -E 's|tcp://localhost:1317|tcp://localhost:1315|g' $VALIDATOR3_APP_TOML
-# sed -i -E 's|0.0.0.0:9090|0.0.0.0:9086|g' $VALIDATOR3_APP_TOML
-sed -i -E 's|localhost:9090|localhost:9086|g' $VALIDATOR3_APP_TOML
-# sed -i -E 's|0.0.0.0:9091|0.0.0.0:9087|g' $VALIDATOR3_APP_TOML
-sed -i -E 's|localhost:9091|localhost:9087|g' $VALIDATOR3_APP_TOML
+sed -i -E 's|0.0.0.0:9090|0.0.0.0:9086|g' $VALIDATOR3_APP_TOML
+# sed -i -E 's|localhost:9090|localhost:9086|g' $VALIDATOR3_APP_TOML
+sed -i -E 's|0.0.0.0:9091|0.0.0.0:9087|g' $VALIDATOR3_APP_TOML
+# sed -i -E 's|localhost:9091|localhost:9087|g' $VALIDATOR3_APP_TOML
 sed -i -E 's|tcp://0.0.0.0:10337|tcp://0.0.0.0:10357|g' $VALIDATOR3_APP_TOML
 
 # change config.toml values
@@ -95,6 +95,20 @@ sed -i -E 's|allow_duplicate_ip = false|allow_duplicate_ip = true|g' $VALIDATOR3
 sed -i -E 's|prometheus = false|prometheus = true|g' $VALIDATOR3_CONFIG
 sed -i -E 's|prometheus_listen_addr = ":26660"|prometheus_listen_addr = ":26620"|g' $VALIDATOR3_CONFIG
 
+# update
+update_test_genesis () {
+    # EX: update_test_genesis '.consensus_params["block"]["max_gas"]="100000000"'
+    cat $HOME/.terrad/validator1/config/genesis.json | jq "$1" > tmp.json && mv tmp.json $HOME/.terrad/validator1/config/genesis.json
+    cat $HOME/.terrad/validator2/config/genesis.json | jq "$1" > tmp.json && mv tmp.json $HOME/.terrad/validator2/config/genesis.json
+    cat $HOME/.terrad/validator3/config/genesis.json | jq "$1" > tmp.json && mv tmp.json $HOME/.terrad/validator3/config/genesis.json
+}
+
+update_test_genesis '.app_state["gov"]["voting_params"]["voting_period"] = "30s"'
+update_test_genesis '.app_state["mint"]["params"]["mint_denom"]= "uluna"'
+update_test_genesis '.app_state["gov"]["deposit_params"]["min_deposit"]=[{"denom": "uluna","amount": "1000000"}]'
+update_test_genesis '.app_state["crisis"]["constant_fee"]={"denom": "uluna","amount": "1000"}'
+update_test_genesis '.app_state["staking"]["params"]["bond_denom"]="uluna"'
+
 
 # copy validator1 genesis file to validator2-3
 cp $HOME/.terrad/validator1/config/genesis.json $HOME/.terrad/validator2/config/genesis.json
@@ -109,7 +123,26 @@ sed -i -E "s|persistent_peers = \"\"|persistent_peers = \"$node1@localhost:26656
 sed -i -E "s|persistent_peers = \"\"|persistent_peers = \"$node1@localhost:26656,$node2@localhost:26656,$node3@localhost:26656\"|g" $HOME/.terrad/validator3/config/config.toml
 
 
-# # start all three validators
+# # start all three validators/
 screen -S terra1 -t terra1 -d -m terrad start --home=$HOME/.terrad/validator1
 screen -S terra2 -t terra2 -d -m terrad start --home=$HOME/.terrad/validator2
 screen -S terra3 -t terra3 -d -m terrad start --home=$HOME/.terrad/validator3
+
+# ===========
+
+sleep 7
+
+terrad tx wasm store /Users/donglieu/52024/core/scripts/hackatom.wasm --from validator1 --output json --gas auto --gas-adjustment 2.4 --fees 100000000uluna --chain-id testing --home ~/.terrad/validator1 --keyring-backend test -y
+
+terrad tx wasm instantiate 1 '{"verifier":"terra1au3y48s4ezv3j0n77m53h0lf9vnju59vevqvqx","beneficiary":"terra1hxxtn2lhuv9qf2s77llsemtayer5exhmkwh4le"}' --from validator1 --output json --gas auto --gas-adjustment 2.3 --fees 20000000uluna --chain-id testing --home ~/.terrad/validator1 --keyring-backend  test --admin "" -y
+
+terrad tx gov submit-proposal  software-upgrade v4  --upgrade-info v4 --upgrade-height 30  --title upgrade --description upgrade --from validator1 --keyring-backend test --home ~/.terrad/validator1 --chain-id testing --deposit 10000000uluna -y
+
+terrad tx gov vote 1 yes  --from validator1 --keyring-backend test --home ~/.terrad/validator1 --chain-id testing -y 
+
+terrad tx gov vote 1 yes  --from validator2 --keyring-backend test --home ~/.terrad/validator2 --chain-id testing -y 
+
+terrad tx gov vote 1 yes  --from validator3 --keyring-backend test --home ~/.terrad/validator3 --chain-id testing -y 
+
+sleep 7
+

fcb9fb36fb7547040f524462c3457cee52e07c5c
Till Ziegler <tz@schoeneweide.tk>
2024-06-27 09:02:23 +0200

    add upgrade handling

---
+++ app/upgrades/v8_1/upgrades.go
@@ -0,0 +1,25 @@
+package v8_1
+
+import (
+	"github.com/classic-terra/core/v3/app/keepers"
+	"github.com/classic-terra/core/v3/app/upgrades"
+	treasurykeeper "github.com/classic-terra/core/v3/x/treasury/keeper"
+	treasurytypes "github.com/classic-terra/core/v3/x/treasury/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+func CreateV81UpgradeHandler(
+	mm *module.Manager,
+	cfg module.Configurator,
+	_ upgrades.BaseAppParamManager,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+
+		// set default oracle split
+		mm.Modules[treasurytypes.ModuleName].(*treasurykeeper.Keeper).SetOracleSplit(ctx, treasurytypes.DefaultOracleSplit)
+		return mm.RunMigrations(ctx, cfg, fromVM)
+	}
+}

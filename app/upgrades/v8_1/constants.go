514ffb0407864ae0b48ff535599bed0951dd2374
Till Ziegler <tz@schoeneweide.tk>
2024-06-29 08:07:30 +0200

    fix: remove unused store upgrades

--- app/upgrades/v8_1/constants.go
+++ app/upgrades/v8_1/constants.go
@@ -3,9 +3,6 @@ package v8_1
 
 import (
 	"github.com/classic-terra/core/v3/app/upgrades"
-	store "github.com/cosmos/cosmos-sdk/store/types"
-	consensustypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
-	crisistpyes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 )
 
 const UpgradeName = "v8_1"
@@ -13,10 +10,4 @@ const UpgradeName = "v8_1"
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateV81UpgradeHandler,
-	StoreUpgrades: store.StoreUpgrades{
-		Added: []string{
-			consensustypes.ModuleName,
-			crisistpyes.ModuleName,
-		},
-	},
 }

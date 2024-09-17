07a2085bf052787729690db8d65f5d8738ec4bd7
StrathCole <strathcole@gmail.com>
2024-09-17 16:39:36 +0200

    - handle excessive fee deduction

--- custom/auth/ante/expected_keeper.go
+++ custom/auth/ante/expected_keeper.go
@@ -30,6 +30,8 @@ type BankKeeper interface {
 	SendCoins(ctx sdk.Context, from, to sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule string, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
 }
 
 type DistrKeeper interface {

4f2cc589f05d6f469cc56b6f81112a1bf29febce
StrathCole <7449529+StrathCole@users.noreply.github.com>
2024-09-25 06:55:09 +0200

    Bump wasmvm (#522)

--- app/app.go
+++ app/app.go
@@ -57,6 +57,7 @@ import (
 	v8 "github.com/classic-terra/core/v3/app/upgrades/v8"
 	v8_1 "github.com/classic-terra/core/v3/app/upgrades/v8_1"
 	v8_2 "github.com/classic-terra/core/v3/app/upgrades/v8_2"
+	v8_3 "github.com/classic-terra/core/v3/app/upgrades/v8_3"
 
 	customante "github.com/classic-terra/core/v3/custom/auth/ante"
 	custompost "github.com/classic-terra/core/v3/custom/auth/post"
@@ -89,6 +90,7 @@ var (
 		v8.Upgrade,
 		v8_1.Upgrade,
 		v8_2.Upgrade,
+		v8_3.Upgrade,
 	}
 
 	// Forks defines forks to be applied to the network

869d296dd27e4a01cf707363caff74365261c62e
tusoict <tranminhtu.ctv@gmail.com>
2024-10-10 11:23:26 +0700

    add keys_test.go (unitest for keys.go)

---
+++ custom/gov/types/keys_test.go
@@ -0,0 +1,66 @@
+package types
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+)
+
+var addr = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+
+func TestProposalKeys(t *testing.T) {
+	// key proposal
+	key := govtypes.ProposalKey(1)
+	proposalID := govtypes.SplitProposalKey(key)
+	require.Equal(t, int(proposalID), 1)
+
+	// key active proposal queue
+	now := time.Now()
+	key = govtypes.ActiveProposalQueueKey(3, now)
+	proposalID, expTime := govtypes.SplitActiveProposalQueueKey(key)
+	require.Equal(t, int(proposalID), 3)
+	require.True(t, now.Equal(expTime))
+
+	// key inactive proposal queue
+	key = govtypes.InactiveProposalQueueKey(3, now)
+	proposalID, expTime = govtypes.SplitInactiveProposalQueueKey(key)
+	require.Equal(t, int(proposalID), 3)
+	require.True(t, now.Equal(expTime))
+
+	// invalid key
+	require.Panics(t, func() { govtypes.SplitProposalKey([]byte("test")) })
+	require.Panics(t, func() { govtypes.SplitInactiveProposalQueueKey([]byte("test")) })
+}
+
+func TestDepositKeys(t *testing.T) {
+	key := govtypes.DepositsKey(2)
+	proposalID := govtypes.SplitProposalKey(key)
+	require.Equal(t, int(proposalID), 2)
+
+	key = govtypes.DepositKey(2, addr)
+	proposalID, depositorAddr := govtypes.SplitKeyDeposit(key)
+	require.Equal(t, int(proposalID), 2)
+	require.Equal(t, addr, depositorAddr)
+}
+
+func TestVoteKeys(t *testing.T) {
+	key := govtypes.VotesKey(2)
+	proposalID := govtypes.SplitProposalKey(key)
+	require.Equal(t, int(proposalID), 2)
+
+	key = govtypes.VoteKey(2, addr)
+	proposalID, voterAddr := govtypes.SplitKeyDeposit(key)
+	require.Equal(t, int(proposalID), 2)
+	require.Equal(t, addr, voterAddr)
+}
+
+func TestTotalDepositKeys(t *testing.T) {
+	key := TotalDepositKey(2)
+	proposalID := govtypes.SplitProposalKey(key)
+	require.Equal(t, int(proposalID), 2)
+}

fb0f953ae643719abf0fdbb1316f672542b45555
tusoict <tranminhtu.ctv@gmail.com>
2024-10-13 12:23:44 +0700

    Update codec & fix config test

--- custom/gov/types/v2lunc1/codec.go
+++ custom/gov/types/v2lunc1/codec.go
@@ -6,10 +6,7 @@ import (
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
-	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
-	govcodec "github.com/cosmos/cosmos-sdk/x/gov/codec"
 	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
-	groupcodec "github.com/cosmos/cosmos-sdk/x/group/codec"
 )
 
 // RegisterLegacyAminoCodec registers all the necessary types and interfaces for the
@@ -48,11 +45,3 @@ var (
 	// defined at the application level.
 	ModuleCdc = codec.NewAminoCodec(amino)
 )
-
-func init() {
-	// Register all Amino interfaces and concrete types on the authz and gov Amino codec so that this can later be
-	// used to properly serialize MsgGrant, MsgExec and MsgSubmitProposal instances
-	RegisterLegacyAminoCodec(authzcodec.Amino)
-	RegisterLegacyAminoCodec(govcodec.Amino)
-	RegisterLegacyAminoCodec(groupcodec.Amino)
-}

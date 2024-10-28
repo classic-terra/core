3914bc8c47a923cd86bef87f653154ace40b2beb
tusoict <tranminhtu.ctv@gmail.com>
2024-10-28 11:59:36 +0700

    revert register code from gov in others module

--- custom/params/types/codec.go
+++ custom/params/types/codec.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	govtypes "github.com/classic-terra/core/v3/custom/gov/types/v2lunc1"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/params/types/proposal"
 )
@@ -8,4 +9,8 @@ import (
 // RegisterLegacyAminoCodec registers all necessary param module types with a given LegacyAmino codec.
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&proposal.ParameterChangeProposal{}, "params/ParameterChangeProposal", nil)
+}
+
+func init() {
+	govtypes.RegisterProposalTypeCodec(&proposal.ParameterChangeProposal{}, "params/ParameterChangeProposal")
 }
--- custom/upgrade/types/codec.go
+++ custom/upgrade/types/codec.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	govtypes "github.com/classic-terra/core/v3/custom/gov/types/v2lunc1"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
@@ -10,4 +11,9 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(types.Plan{}, "upgrade/Plan", nil)
 	cdc.RegisterConcrete(&types.SoftwareUpgradeProposal{}, "upgrade/SoftwareUpgradeProposal", nil)
 	cdc.RegisterConcrete(&types.CancelSoftwareUpgradeProposal{}, "upgrade/CancelSoftwareUpgradeProposal", nil)
+}
+
+func init() {
+	govtypes.RegisterProposalTypeCodec(&types.SoftwareUpgradeProposal{}, "upgrade/SoftwareUpgradeProposal")
+	govtypes.RegisterProposalTypeCodec(&types.CancelSoftwareUpgradeProposal{}, "upgrade/CancelSoftwareUpgradeProposal")
 }

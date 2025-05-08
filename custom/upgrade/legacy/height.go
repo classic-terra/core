da6716f82b5fbf3b36d72aedfad8b2c563218c69
StrathCole <strathcole@gmail.com>
2025-05-08 23:36:37 +0200

    gofumpt

--- custom/upgrade/legacy/height.go
+++ custom/upgrade/legacy/height.go
@@ -5,8 +5,8 @@ import core "github.com/classic-terra/core/v3/types"
 const (
 	MainnetUpgradeHeight  = int64(18303000) // columbus-5 mainnet upgrade height to v8
 	TestnetUpgradeHeight  = int64(19354000) // rebel-2 testnet upgrade height to v8
-	LegacyUpgradeHeight = int64(25)       // Local testing upgrade height to v8 (using update-test-multi.sh script)
-	LocalnetUpgradeHeight = int64(0)       // Local testing or automated workflows without pre-v8 data
+	LegacyUpgradeHeight   = int64(25)       // Local testing upgrade height to v8 (using update-test-multi.sh script)
+	LocalnetUpgradeHeight = int64(0)        // Local testing or automated workflows without pre-v8 data
 )
 
 // GetUpgradeHeight returns the appropriate upgrade height based on the chain ID

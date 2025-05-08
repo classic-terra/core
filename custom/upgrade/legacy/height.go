ed08d9d9f36f384f7f6468a0202ef4edefbdfa9d
StrathCole <strathcole@gmail.com>
2025-05-08 21:49:21 +0200

    - add missing files

---
+++ custom/upgrade/legacy/height.go
@@ -0,0 +1,22 @@
+package legacy
+
+import core "github.com/classic-terra/core/v3/types"
+
+const (
+	MainnetUpgradeHeight  = int64(18303000) // columbus-5 mainnet upgrade height to v8
+	TestnetUpgradeHeight  = int64(19354000) // rebel-2 testnet upgrade height to v8
+	LocalnetUpgradeHeight = int64(25)       // Local testing upgrade height to v8 (using update-test-multi.sh script)
+)
+
+// GetUpgradeHeight returns the appropriate upgrade height based on the chain ID
+func GetUpgradeHeight(chainID string) int64 {
+	switch chainID {
+	case core.ColumbusChainID:
+		return MainnetUpgradeHeight
+	case core.RebelChainID:
+		return TestnetUpgradeHeight
+	default:
+		// For local testing or other networks
+		return LocalnetUpgradeHeight
+	}
+}

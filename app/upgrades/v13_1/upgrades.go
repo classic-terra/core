1ba316c97f487fc143d726f75ca4b87f4ee3399c
Kien Trinh <51135161+kien6034@users.noreply.github.com>
2025-12-07 09:56:24 +0700

    test: add test wamd (#72)
    * feat: add upgrade handler (#615)
    
    * bet
    
    * test: validate wasmd 1.x wamsd binary still works through migration
    
    * bet
    
    * bet
    
    * bet

---
+++ app/upgrades/v13_1/upgrades.go
@@ -0,0 +1,22 @@
+//nolint:revive
+package v13_1
+
+import (
+	"context"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/classic-terra/core/v3/app/keepers"
+	"github.com/classic-terra/core/v3/app/upgrades"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+func CreateV131UpgradeHandler(
+	mm *module.Manager,
+	cfg module.Configurator,
+	_ upgrades.BaseAppParamManager,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return mm.RunMigrations(ctx, cfg, fromVM)
+	}
+}

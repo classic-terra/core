3bc15eb9883ff192232a7bd9a86d56f55a59e153
kien6034 <trinhtrungkientb99@gmail.com>
2026-01-06 16:22:23 +0700

    upgrade 13_1

--- upgrade.sh
+++ upgrade.sh
@@ -1,13 +1,13 @@
-SOFTWARE_UPGRADE_NAME="v11"
-UPGRADE_HEIGHT=22826069
+SOFTWARE_UPGRADE_NAME="v13_1"
+UPGRADE_HEIGHT=27963601
 
 UPGRADE_INFO=$(jq -n '
   {
       "binaries": {
-          "linux/amd64": "https://github.com/classic-terra/core/releases/download/v3.4.0-rc.0/terra_3.4.0-rc.0_Linux_x86_64.tar.gz?checksum=sha256:2402fca560086e35693eb7dfd3f1e7a0923475c4568719ab2f0bcf7ee91b4f3b",
+          "linux/amd64": "https://github.com/classic-terra/core/releases/download/v3.6.1-rc.0/terra_3.6.1-rc.0_Linux_x86_64.tar.gz?checksum=sha256:543dd9d8a65c96ab8d98d3cf49f99035a8bfd474d30d738b68d1032144c9a243",
       }
   }')
 
 
 
-terrad tx gov submit-legacy-proposal software-upgrade "$SOFTWARE_UPGRADE_NAME" --upgrade-height $UPGRADE_HEIGHT --upgrade-info "$UPGRADE_INFO" --title "Upgrade to v11" --description "Orbit: Upgrade regarding the first part of the unforking proposal"  --from orbit-testnet --keyring-backend file --chain-id "rebel-2" --gas-prices 30uluna --gas 3000000 --node https://rpc.luncblaze.com:443
+terrad tx gov submit-legacy-proposal software-upgrade "$SOFTWARE_UPGRADE_NAME" --upgrade-height $UPGRADE_HEIGHT --upgrade-info "$UPGRADE_INFO" --title "Upgrade to v13_1" --description "Upgrade to v13_1"  --from orbit-testnet --keyring-backend os --chain-id "rebel-2" --gas-prices 30uluna --gas 3000000 --node https://rpc.luncblaze.com:443

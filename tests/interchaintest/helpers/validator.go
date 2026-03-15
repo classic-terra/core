e42d37a8248e243f3cb8afbd74002553f8eb8b43
Khanh Hoa <hoa@notional.ventures>
2026-03-15 18:38:27 +0700

    bump cosmos-sdk to v0.53.6

--- tests/interchaintest/helpers/validator.go
+++ tests/interchaintest/helpers/validator.go
@@ -6,10 +6,9 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/cosmos/cosmos-sdk/types/module/testutil"
-
 	sdked25519 "github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/cosmos/cosmos-sdk/types/module/testutil"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 

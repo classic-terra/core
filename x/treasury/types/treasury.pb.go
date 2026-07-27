880862bc20f85d8dff06a3f7be9ba19bc2159ac0
StrathCole <strathcole@gmail.com>
2026-07-27 15:09:41 +0200

    (fix): Regenerate oracle and treasury proto

--- x/treasury/types/treasury.pb.go
+++ x/treasury/types/treasury.pb.go
@@ -4,17 +4,16 @@
 package types
 
 import (
-	fmt "fmt"
-	io "io"
-	math "math"
-	math_bits "math/bits"
-
 	cosmossdk_io_math "cosmossdk.io/math"
+	fmt "fmt"
 	_ "github.com/cosmos/cosmos-proto"
 	github_com_cosmos_cosmos_sdk_types "github.com/cosmos/cosmos-sdk/types"
 	types "github.com/cosmos/cosmos-sdk/types"
 	_ "github.com/cosmos/gogoproto/gogoproto"
 	proto "github.com/cosmos/gogoproto/proto"
+	io "io"
+	math "math"
+	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
